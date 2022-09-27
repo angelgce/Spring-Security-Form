92ad9daba8285eae73acb732b4b9b973f61f744b
Angel Chavez <angelgce.chavez@gmail.com>
2022-09-26 21:47:16 -0500

    update security

--- src/main/java/com/example/login/app/models/security/ResourceServerConfig.java
+++ src/main/java/com/example/login/app/models/security/ResourceServerConfig.java
@@ -1,10 +1,21 @@
 package com.example.login.app.models.security;
 
+import java.util.Arrays;
+
+import javax.persistence.criteria.Order;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -14,8 +25,29 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     // #12 add all the publics endpoints
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user", "/api/user/{username}","/api/role").permitAll()
-                .anyRequest().authenticated();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user", "/api/user/{username}", "/api/role")
+                .permitAll()
+                .anyRequest().authenticated().and().cors().configurationSource(corsConfigurationSource());
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(
+                new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
